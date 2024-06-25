f350bee8ed36efea1b5d5b93daa57d3b88b9b5dc
MaximilianoEgidi <maximilianoegidi@gmail.com>
2024-06-25 00:09:56 -0300

    1.05
    Se implemento la interfaz grafica de forma satisfactoria

--- Apagar.py
+++ Apagar.py
@@ -2,9 +2,11 @@ import os
 import time
 import requests
 import webbrowser
-import tkinter
+import tkinter as tk
+from tkinter import font
+from tkinter import messagebox
 
-version="v1.0.4"
+version="v1.0.5"
 
 os.system('cls')
 print(f"Iniciando PythonAutoShutdown {version} ")
@@ -54,47 +56,62 @@ time.sleep(3)
 os.system('cls')
 
 
+# Programa principal
+def apagar():
+
+    tiempo=float(textoTiempo.get())
+
+    if var.get() == 1:
+        segundos=tiempo*3600
+        unidad = "horas"
+    elif var.get() == 2:
+        unidad = "minutos"
+        segundos=tiempo*60
+
+    messagebox.showinfo("Alerta", f"La PC se apagara en {tiempo} {unidad}")
+
+    time.sleep(2)
+    ventana.withdraw()
+    
+    while (segundos>0):
+        segundos-=1
+        print(segundos)
+        time.sleep(1)
+
+        if (segundos<=0):
+             os.system(f"shutdown /s /t 1")
+             break
 
 # Ventana
 
+ventana = tk.Tk()
+ventana.geometry("400x300")
+ventana.title("Python Auto Shutdown")
 
 
-# Programa principal
-
-unidad = ""
-while unidad != "H" and unidad != "M":
-    unidad = input("Horas o Minutos (H o M): ").upper()
-    os.system("cls")
-    if unidad != "H" and unidad != "M":
-        print("Entrada inválida. Por favor, ingresa 'H' para Horas o 'M' para Minutos.")
-        time.sleep(4)
-        os.system("cls")
-
-if (unidad=="H"):
-    unidad="Horas"
-else:
-    if(unidad=="M"):
-        unidad="Minutos"
-
-if (unidad=="Horas"):    
-    horas = float(input(f"En cuantas horas apagar la computadora?"))
-else:
-    if(unidad=="Minutos"):
-        horas = float(input(f"En cuantos minutos apagar la computadora?"))
+fuente_titulo = font.Font(family="Helvetica", size=22, weight="bold")
+fuente_subtitulo = font.Font(family="Helvetica", size=12, weight="bold")
+fuente_texto = font.Font(family="Helvetica", size=12, weight="bold")
 
 
-if (unidad == "Horas"):
-    segundos = horas * 3600
-else:
-    segundos = horas * 60  
+presentacion = tk.Label(ventana, text="Python Auto Shutdown", font=fuente_titulo, fg="blue")
+presentacion.pack(pady=10)
 
-print(f"La PC se apagará en {horas} {unidad}")
-while (segundos>0):
-    segundos-=1
-    print(segundos)
-    time.sleep(1)
-    if (segundos<=0):
-         os.system(f"shutdown /s /t 1")
-         break  
-        
 
+var = tk.IntVar(value=1)
+checkBoxHoras = tk.Radiobutton(ventana, text="Horas", variable=var, value=1, font=fuente_texto)
+checkBoxMinutos = tk.Radiobutton(ventana, text="Minutos", variable=var, value=2, font=fuente_texto)
+checkBoxHoras.pack(pady=2)
+checkBoxMinutos.pack(pady=2)
+
+presentacion = tk.Label(ventana, text="Ingrese un valor", font=fuente_subtitulo, fg="black")
+presentacion.pack(pady=10)
+textoTiempo = tk.Entry(ventana, font=fuente_texto)
+textoTiempo.pack(pady=10)
+
+
+botonEnviar = tk.Button(ventana, text="Enviar", command=apagar, bg="lightblue", font=fuente_texto)
+botonEnviar.pack(pady=5)
+
+
+ventana.mainloop()
--- Ventana.py
+++ Ventana.py
@@ -2,39 +2,41 @@ import tkinter as tk
 from tkinter import font
 from tkinter import messagebox
 
-# Crear ventana
+
 ventana = tk.Tk()
 ventana.geometry("400x300")
 ventana.title("Python Auto Shutdown")
 
-# Establecer fuente
-fuente_titulo = font.Font(family="Helvetica", size=16, weight="bold")
-fuente_texto = font.Font(family="Helvetica", size=12)
 
-# Presentación
+fuente_titulo = font.Font(family="Helvetica", size=22, weight="bold")
+fuente_subtitulo = font.Font(family="Helvetica", size=12, weight="bold")
+fuente_texto = font.Font(family="Helvetica", size=12, weight="bold")
+
+
 presentacion = tk.Label(ventana, text="Python Auto Shutdown", font=fuente_titulo, fg="blue")
 presentacion.pack(pady=10)
 
-# Función saludo
-def saludo():
+
+def apagar():
     tiempo = textoTiempo.get()
     print(f"Hola, el tiempo ingresado es: {tiempo}")
     messagebox.showinfo("Alerta", f"Los datos de tiempo son incorrectos")
 
-# Opciones de tiempo
+
 var = tk.IntVar(value=1)
 checkBoxHoras = tk.Radiobutton(ventana, text="Horas", variable=var, value=1, font=fuente_texto)
 checkBoxMinutos = tk.Radiobutton(ventana, text="Minutos", variable=var, value=2, font=fuente_texto)
 checkBoxHoras.pack(pady=2)
 checkBoxMinutos.pack(pady=2)
 
-# Entrada de tiempo
+presentacion = tk.Label(ventana, text="Ingrese un valor", font=fuente_subtitulo, fg="black")
+presentacion.pack(pady=10)
 textoTiempo = tk.Entry(ventana, font=fuente_texto)
 textoTiempo.pack(pady=10)
 
-# Botón Enviar
-botonEnviar = tk.Button(ventana, text="Enviar", command=saludo, bg="lightblue", font=fuente_texto)
+
+botonEnviar = tk.Button(ventana, text="Enviar", command=apagar, bg="lightblue", font=fuente_texto)
 botonEnviar.pack(pady=5)
 
-# Iniciar bucle de eventos
+
 ventana.mainloop()
